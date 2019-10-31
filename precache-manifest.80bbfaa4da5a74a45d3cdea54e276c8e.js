b995fd9c17c898a969c76c500e74c6dc55274a1a
suman saurav <suman94310@gmail.com>
2019-10-31 23:31:18 +0530

    Updates

---
+++ precache-manifest.80bbfaa4da5a74a45d3cdea54e276c8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ca63071460b12223f857038a40c9e7",
+    "url": "/algosite/index.html"
+  },
+  {
+    "revision": "31f3b94bb03e39b1c3f1",
+    "url": "/algosite/static/css/main.50204638.chunk.css"
+  },
+  {
+    "revision": "927f869172ee18aeaa66",
+    "url": "/algosite/static/js/2.15b49a4b.chunk.js"
+  },
+  {
+    "revision": "31f3b94bb03e39b1c3f1",
+    "url": "/algosite/static/js/main.a6001a6c.chunk.js"
+  },
+  {
+    "revision": "641251a7cce2ba000830",
+    "url": "/algosite/static/js/runtime-main.4813e916.js"
+  },
+  {
+    "revision": "69e7cd5020e564c56ac55b76a6821dd8",
+    "url": "/algosite/static/media/home-background.69e7cd50.jpg"
+  }
+]);
